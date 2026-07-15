8bf8b8f2251b1ccf991d882f7baa73ea753b66e4
i343281 <samuel.davies@sap.com>
2026-07-15 17:37:54 +0100

    feat: add grep

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,59 @@ use std::fs;
 pub fn read_file_content(file_path: &str) -> String {
     return fs::read_to_string(&file_path).expect("File should exist");
 }
+
+pub fn count_stats(content: &str) -> (usize, usize, usize) {
+    let lines = content.lines().count();
+    let words = content.split_whitespace().count();
+    let chars = content.chars().count();
+
+    (lines, words, chars)
+}
+
+pub fn grep_lines<'a>(pattern: &str, content: &'a str) -> Vec<&'a str> {
+    let found_lines: &mut Vec<&str> = &mut Vec::<&str>::new();
+    for line in content.lines() {
+        if line.contains(pattern) {
+            found_lines.push(line);
+        }
+    }
+    found_lines.to_vec()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::count_stats;
+
+    #[test]
+    fn counts_a_simple_string() {
+        assert_eq!(count_stats("Hello\nworld"), (2, 2, 11));
+    }
+
+    #[test]
+    fn empty_string_is_all_zero() {
+        assert_eq!(count_stats(""), (0, 0, 0));
+    }
+}
+
+#[cfg(test)]
+mod grep_tests {
+    use super::*;
+
+    #[test]
+    fn find_matching_lines() {
+        let text = "banana\napple\ngrapefruit";
+        assert_eq!(grep_lines("apple", text), vec!["apple"])
+    }
+
+    #[test]
+    fn no_match_returns_empty() {
+        let text = "apple\nbanana";
+        assert_eq!(grep_lines("orange", text), Vec::<&str>::new())
+    }
+
+    #[test]
+    fn case_sensitive_by_default() {
+        let text = "Apple\napple";
+        assert_eq!(grep_lines("apple", text), vec!["apple"]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,47 @@
 use std::{env, error::Error, fs, process};
 
-use rq::read_file_content;
+use rq::{grep_lines, read_file_content};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file_path = &args[1];
+    let config = Config::new(&args);
 
-    if let Err(e) = run(&file_path) {
+    if let Err(e) = run(&config.file_path) {
         println!("Application error : {}", e);
         process::exit(1);
     }
 
-    let file_content = read_file_content(&file_path);
+    let file_content = read_file_content(&config.file_path);
 
-    println!("{}", file_content);
-    
+    if config.functionality.eq("grep") {
+        println!(
+            "{} {:?}",
+            &config.functionality,
+            grep_lines(&config.pattern, &file_content)
+        )
+    } else {
+        println!("{}", file_content);
+    }
 }
 
 fn run(file_path: &str) -> Result<(), Box<dyn Error>> {
     fs::read_to_string(file_path)?;
     Ok(())
- }
+}
+
+struct Config {
+    functionality: String,
+    pattern: String,
+    file_path: String,
+}
+
+impl Config {
+    // TODO - bug here - no support for variable argument length. Use clap
+    pub fn new(args: &Vec<String>) -> Self {
+        Self {
+            functionality: args[2].to_string(),
+            pattern: args[3].to_string(),
+            file_path: args[4].to_string(),
+        }
+    }
+}
